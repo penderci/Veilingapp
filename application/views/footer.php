fbe586e05f9242e34f64c6b056b012e03aa46ac5
Cindy Penders <cindy.penders@hotmail.com>
2015-03-27 12:39:36 +0100

    CI en Angular geinstalleerd, template gemaakt

---
+++ application/views/footer.php
@@ -0,0 +1,7 @@
+<div class="navbar navbar-fixed-bottom">
+    <p class="text-muted text-center">Copyright &copy; : Cindy Penders 2015 </p>
+</div>
+
+
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html ng-app="veilingapp">
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Veilingadministratie</title>
+    <link href="./assets/css/bootstrap.css" rel="stylesheet">
+<!--    <link href="./bootstrap/css/bootstrap.css" rel="stylesheet">-->
+    <!--//maxcdn.bootstrapcdn.com/bootswatch/3.3.2/sandstone/bootstrap.min.css /bootstrap/css/bootstrap.min.css -->
+
+</head>
+<body>
+<div class="navbar navbar-default navbar-fixed-top" style="text-align:center">
+    <h1 class="navbar-header" style="color:white">Veilingadministratie</h1>
+</div>
+<h1>Veilingadministratie</h1>
